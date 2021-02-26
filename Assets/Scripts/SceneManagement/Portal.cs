14bb6beb7abdfce6e160f8722b19c7eee7b64758
Hydeo <p.garciapelayo@gmail.com>
2021-02-26 18:40:10 +0000

    [CORE] Portals spawning logic

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 namespace RPG.SceneManagement
@@ -8,6 +10,7 @@ namespace RPG.SceneManagement
     public class Portal : MonoBehaviour
     {
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] GameObject spawnPoint;
         private void OnTriggerEnter(Collider other)
         {
             if(other.tag == "Player")
@@ -20,8 +23,27 @@ namespace RPG.SceneManagement
         {
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
-            Debug.Log("Loading new scene");
+            Portal portal = GetOtherPortal();
+            UpdatePlayer(portal);
             Destroy(gameObject);
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach(Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                return portal;
+            }
+
+            return null;
+        }
+
+        private void UpdatePlayer(Portal otherPortal )
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.transform.position);
+            player.transform.rotation = otherPortal.spawnPoint.transform.rotation;
         }
     }
 }
